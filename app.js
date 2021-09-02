42055c02b9974418f3555599ae9b3b82ddd7d36f
arif1193 <rahman1193arif@gmail.com>
2021-09-02 23:41:39 +0600

    nice

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const searchBook = () => {
     const searchFieldText = inputField.value;
     inputField.value = '';
 
+    // Error massage and Handle
     if (searchFieldText === "") {
         displayError();
     }
@@ -19,6 +20,8 @@ const searchBook = () => {
     }
 
 }
+
+// get error massage function
 const displayError = () => {
     document.getElementById('error-massage').style.display = "block";
     document.getElementById('total-books').style.display = "none";
@@ -26,36 +29,36 @@ const displayError = () => {
 
 }
 
+// get book search result
+
 const displayBookSelf = selfs => {
     document.getElementById('total-books').innerText = "";
     document.getElementById('error-massage').style.display = "block";
-    // console.log(self)
     const searchBookResult = document.getElementById('search-book-result');
     searchBookResult.textContent = "";
     const bookList = selfs.docs;
 
+    // error Massage and handle
     if (selfs.numFound === 0) {
         displayError();
     }
     else {
-        // console.log(bookList.length);
         document.getElementById('error-massage').style.display = "none";
         document.getElementById('total-books').innerText = `Total Book ${bookList.length}`;
 
-
+        // loop use(forEach)
         bookList.forEach(self => {
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
                  <div class="card h-100">
                     <div>
-                    <img id="display-image" src="https://covers.openlibrary.org/b/id/${self.cover_i}-L.jpg" class="card-img-top" alt="">
+                    <img id="display-image"  src="https://covers.openlibrary.org/b/id/${self.cover_i}-L.jpg" class="card-img-top" alt="">
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <h5 class="card-title">Book NAme: ${self.title}</h5>
-                        <h5 class="card-title">Author NAme: ${self.author_name}</h5>
-                        <h5 class="card-title">Publisher: ${self.first_publish_year}</h5>
+                        <h5 class="card-title text-center">Book: <br>${self.title}</h5> <hr>
+                        <h5 class="card-title text-center">Author: <br> ${self.author_name}</h5> <hr>
+                        <h5 class="card-title text-center"><u>Publisher</u>: ${self.first_publish_year}</h5>
                         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
                             additional content. This content is a little bit longer.</p>
                     </div>
@@ -66,7 +69,3 @@ const displayBookSelf = selfs => {
     }
 
 }
-
-// const imageResult = images => {
-//     // console.log(images)
-// }
